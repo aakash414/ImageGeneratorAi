0375f4a43d271d64e978033737ebc5b55de8cb38
aakash414 <67820270+aakash414@users.noreply.github.com>
2023-08-13 09:07:51 +0530

    bug fix

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import { Configuration, OpenAiApi } from "openai";
+import { Configuration, OpenAIApi } from "openai";
 import PostSchema from "../mongodb/models/post.js";
 
 dotenv.config();
 
 const router = express.Router();
+
+export default router;
